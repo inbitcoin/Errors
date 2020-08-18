492f89299d9e7993ce6d1dcf207615746d728dcf
Zoe Faltibà <zoe@inbitcoin.it>
2020-08-18 19:53:05 +0200

    linted code + add lint job to CI

--- test/test.js
+++ test/test.js
@@ -8,14 +8,16 @@ var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
 
-var placeHolderMiddleware = function (req, res, next) { next() }
+var placeHolderMiddleware = function (req, res, next) {
+  next()
+}
 var requestIdMiddleware = function (req, res, next) {
   req.headers['X-Request-Id'] = 'request-id-1234'
   next()
 }
 var dynamicRequestIdMiddleware = DynamicMiddleware.create(placeHolderMiddleware)
 var errorHandlerProduction = errorHandler() // default, unless explicitly process.env.NODE_ENV = 'development'
-var errorHandlerDevelopment = errorHandler({env: 'development'})
+var errorHandlerDevelopment = errorHandler({ env: 'development' })
 var dynamicErrorHandler = DynamicMiddleware.create(errorHandlerProduction)
 
 app.use(dynamicRequestIdMiddleware.handler())
@@ -33,22 +35,24 @@ app.get('/cc_error', function (req, res, next) {
   if (!req.query || req.query.explain !== 'true') {
     return next(new errors.ValidationError())
   }
-  next(new errors.ValidationError({
-    explanation: 'The given address is invalid'
-  }))
+  next(
+    new errors.ValidationError({
+      explanation: 'The given address is invalid',
+    })
+  )
 })
 app.use(dynamicErrorHandler.errorHandler())
 
 describe('Test error of correct format - production', function () {
   it('string error', function (done) {
     request(app)
-			.get('/string_error')
-			.expect(500)
-			.end(function (err, res) {
+      .get('/string_error')
+      .expect(500)
+      .end(function (err, res) {
         if (err) return done(err)
         assert.deepEqual(res.body, {
           status: 500,
-          message: 'Some Error'
+          message: 'Some Error',
         })
         done()
       })
@@ -56,141 +60,140 @@ describe('Test error of correct format - production', function () {
 
   it('array error', function (done) {
     request(app)
-			.get('/array_error')
-			.expect(500)
-			.end(function (err, res) {
+      .get('/array_error')
+      .expect(500)
+      .end(function (err, res) {
         if (err) return done(err)
         assert.deepEqual(res.body, {
           status: 500,
-          message: 'Some Error'
+          message: 'Some Error',
         })
         done()
       })
-	})
+  })
 
   it('raw error', function (done) {
     request(app)
-			.get('/error')
-			.expect(500)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.deepEqual(res.body, {
+      .get('/error')
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.deepEqual(res.body, {
           status: 500,
-          message: 'Internal server error'
-				})
-				done()
-			})
-	})
+          message: 'Internal server error',
+        })
+        done()
+      })
+  })
 
   it('Colored-Coins error', function (done) {
     request(app)
-			.get('/cc_error')
-			.expect(400)
-			.end(function (err, res) {
+      .get('/cc_error')
+      .expect(400)
+      .end(function (err, res) {
         if (err) return done(err)
-				assert.deepEqual(res.body, {
+        assert.deepEqual(res.body, {
           name: 'ValidationError',
           code: 10000,
           status: 400,
-          message: 'Validation error'
-				})
-				done()
-			})
-	})
+          message: 'Validation error',
+        })
+        done()
+      })
+  })
 
-	it('Colored-Coins error, with explanation', function (done) {
+  it('Colored-Coins error, with explanation', function (done) {
     request(app)
-			.get('/cc_error?explain=true')
-			.expect(400)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.deepEqual(res.body, {
-					name: 'ValidationError',
-					code: 10000,
-					status: 400,
-					explanation: 'The given address is invalid',
-					message: 'Validation error'
-				})
-				done()
-			})
-	})
+      .get('/cc_error?explain=true')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.deepEqual(res.body, {
+          name: 'ValidationError',
+          code: 10000,
+          status: 400,
+          explanation: 'The given address is invalid',
+          message: 'Validation error',
+        })
+        done()
+      })
+  })
 })
 
 describe('Test error of correct format - development', function () {
-
-	before(function () {
-		dynamicErrorHandler.replace(errorHandlerDevelopment)
-	})
+  before(function () {
+    dynamicErrorHandler.replace(errorHandlerDevelopment)
+  })
 
   it('string error', function (done) {
     request(app)
-			.get('/string_error')
-			.expect(500)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.equal(res.body.status, 500)
-				assert.equal(res.body.message, 'Some Error')
-				assert.notEqual(res.body.stack, undefined)
-				done()
-			})
-	})
+      .get('/string_error')
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.equal(res.body.status, 500)
+        assert.equal(res.body.message, 'Some Error')
+        assert.notEqual(res.body.stack, undefined)
+        done()
+      })
+  })
 
-	it('array error', function (done) {
+  it('array error', function (done) {
     request(app)
-			.get('/array_error')
-			.expect(500)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.equal(res.body.status, 500)
-				assert.equal(res.body.message, 'Some Error')
-				assert.notEqual(res.body.stack, undefined)
-				done()
-			})
-	})
+      .get('/array_error')
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.equal(res.body.status, 500)
+        assert.equal(res.body.message, 'Some Error')
+        assert.notEqual(res.body.stack, undefined)
+        done()
+      })
+  })
 
-	it('raw error', function (done) {
+  it('raw error', function (done) {
     request(app)
-			.get('/error')
-			.expect(500)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.equal(res.body.status, 500)
-				assert.equal(res.body.message, 'Internal server error')
-				assert.notEqual(res.body.original, undefined)
-				done()
-			})
-	})
+      .get('/error')
+      .expect(500)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.equal(res.body.status, 500)
+        assert.equal(res.body.message, 'Internal server error')
+        assert.notEqual(res.body.original, undefined)
+        done()
+      })
+  })
 
-	it('Colored-Coins error', function (done) {
+  it('Colored-Coins error', function (done) {
     request(app)
-			.get('/cc_error')
-			.expect(400)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.equal(res.body.status, 400)
-				assert.equal(res.body.name, 'ValidationError')
-				assert.equal(res.body.code, 10000)
-				assert.equal(res.body.message, 'Validation error')
-				assert.notEqual(res.body.stack, undefined)
-				done()
-			})
-	})
+      .get('/cc_error')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.equal(res.body.status, 400)
+        assert.equal(res.body.name, 'ValidationError')
+        assert.equal(res.body.code, 10000)
+        assert.equal(res.body.message, 'Validation error')
+        assert.notEqual(res.body.stack, undefined)
+        done()
+      })
+  })
 
-	it('Colored-Coins error with explanation', function (done) {
+  it('Colored-Coins error with explanation', function (done) {
     request(app)
-			.get('/cc_error?explain=true')
-			.expect(400)
-			.end(function (err, res) {
-				if (err) return done(err)
-				assert.equal(res.body.status, 400)
-				assert.equal(res.body.name, 'ValidationError')
-				assert.equal(res.body.code, 10000)
-				assert.equal(res.body.message, 'Validation error')
-				assert.equal(res.body.explanation, 'The given address is invalid')
-				assert.notEqual(res.body.stack, undefined)
-				done()
-			})
-	})
+      .get('/cc_error?explain=true')
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err)
+        assert.equal(res.body.status, 400)
+        assert.equal(res.body.name, 'ValidationError')
+        assert.equal(res.body.code, 10000)
+        assert.equal(res.body.message, 'Validation error')
+        assert.equal(res.body.explanation, 'The given address is invalid')
+        assert.notEqual(res.body.stack, undefined)
+        done()
+      })
+  })
 
   it('Colored-Coins error with request-id', function (done) {
     dynamicRequestIdMiddleware.replace(requestIdMiddleware)
